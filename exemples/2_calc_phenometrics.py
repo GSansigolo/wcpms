608684e73cddc9ea83e6fbbcf97a94cc01d5db4e
GSansigolo <gabrielsansigolo@gmail.com>
2024-04-17 16:52:31 -0300

    wip exemple

--- exemples/2_calc_phenometrics.py
+++ exemples/2_calc_phenometrics.py
@@ -15,25 +15,22 @@ config = params_phenometrics(
 print(config)
 
 np.random.seed(0)
-temperature = 15 + 8 * np.random.randn(2, 2, 3)
+temperature = 15 + 8 * np.random.randn(3, 2, 2)
 lon = [[-99.83, -99.32], [-99.79, -99.23]]
 lat = [[42.25, 42.21], [42.63, 42.59]]
 time = pd.date_range("2014-09-06", periods=3)
 reference_time = pd.Timestamp("2014-09-05")
 
-da = xr.DataArray(
-    data=temperature,
-    dims=["x", "y", "time"],
+da = xr.Dataset(
+    data_vars=dict(
+        veg_index=(["time", "x", "y"], temperature),
+    ),
     coords=dict(
-        lon=(["x", "y"], lon),
-        lat=(["x", "y"], lat),
         time=time,
-        reference_time=reference_time,
-    ),
-    attrs=dict(
-        description="Ambient temperature.",
-        units="degC",
-    ),
+        y=(["x", "y"], lat),
+        x=(["x", "y"], lon),
+        spatial_ref=3577,
+    )
 )
 
 print(da)
@@ -49,22 +46,6 @@ Coordinates:
     spatial_ref  int32 3577
 Data variables:
     veg_index    (time, y, x) float32 0.1249737 0.11653577 ... 0.11687457
----------------------------------------------------------------------------------------------
-<xarray.DataArray (x: 2, y: 2, time: 3)> Size: 96B
-array([[[29.11241877, 18.20125767, 22.82990387],
-        [32.92714559, 29.94046392,  7.18177696]],
-
-       [[22.60070734, 13.78914233, 14.17424919],
-        [18.28478802, 16.15234857, 26.63418806]]])
-Coordinates:
-    lon             (x, y) float64 32B -99.83 -99.32 -99.79 -99.23
-    lat             (x, y) float64 32B 42.25 42.21 42.63 42.59
-  * time            (time) datetime64[ns] 24B 2014-09-06 2014-09-07 2014-09-08
-    reference_time  datetime64[ns] 8B 2014-09-05
-Dimensions without coordinates: x, y
-Attributes:
-    description:  Ambient temperature.
-    units:        degC
 ---------------------------------------------------------------------------------------------
 '''
 
