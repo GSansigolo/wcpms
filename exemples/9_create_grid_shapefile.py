b87bf83599ff06fe11e7dd2d42d478862a6413ef
GSansigolo <gabrielsansigolo@gmail.com>
2024-05-16 17:12:04 -0300

    fix repo

--- exemples/9_create_grid_shapefile.py
+++ exemples/9_create_grid_shapefile.py
@@ -4,7 +4,7 @@ import os
 path_dir = os.path.dirname(__file__)
 
 points = generate_grid_from_shapefile(
-    shapefile_dir=os.path.join(path_dir, "region/POLYGON.shp"),
+    shapefile_dir=os.path.join(path_dir, "notebooks/region/POLYGON.shp"),
     grid_type='random',
     plot_size=20
 )
